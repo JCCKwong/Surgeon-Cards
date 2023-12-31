7230f3b9be8da74f425281a00fff3b7456c0273a
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2023-12-30 21:54:47 -0500

    Add files via upload

---
+++ App.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import PIL.Image
+from streamlit_gsheets import GSheetsConnection
+
+st.set_page_config(page_title="UofT Uro Surgeon Cards",
+                   page_icon="https://cdn.pixabay.com/photo/2022/09/20/10/27/urology-7467570_1280.png"
+                   )
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read()
+logo = PIL.Image.open(r'images\uofturo.png')
+
+st.title("UofT Urology Surgeon Cards")
+st.write('Developed by Jethro Kwong')
+
+# Search by Procedure
+procedure_list = df['Procedure'].sort_values(ascending=True).unique()
+procedure_select = st.selectbox('Select Procedure', (procedure_list))
+staff_list = df[df['Procedure']==procedure_select]['Staff'].sort_values(ascending=True).unique()
+staff_select = st.selectbox('Select Staff', (staff_list))
+
+submit = st.button("SUBMIT")
+equipment_setup, keysteps, dictation = st.tabs(["Equipment & Setup", "Key Steps", "Dictation"])
+if submit:
+    filtered_df = df[(df['Procedure'] == procedure_select) & (df['Staff'] == staff_select)].reset_index(drop=True)
+
+    with equipment_setup:
+        st.header('Equipment List')
+        st.write(filtered_df['Equipment'][0])
+        st.header('Setup')
+        st.write(filtered_df['Setup'][0])
+
+    with keysteps:
+        st.header("Key Steps")
+        st.write(filtered_df['Key steps'][0])
+
+    with dictation:
+        st.header("Dictation")
+        st.write(filtered_df['Dictation'][0])
+
+st.image(logo, use_column_width=True)
